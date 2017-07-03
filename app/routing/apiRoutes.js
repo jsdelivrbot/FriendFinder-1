20e9717800354034795b138a3f9452dd1f827e9a
Ariene <ariene@Arienes-MacBook-Pro.local>
2017-07-03 14:54:06 -0400

    adding pseudocode for find match logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,41 +1,33 @@
 var express = require('express'),
-    router = express.Router();
-var friends = require("../data/friends");
+    router = express.Router(),
+    friends = require('../data/friends');
 
-var i = 0;
-var user = [];
-
-function compareScore(score) {
-    score.forEach(function(element) {
-        console.log('element: ' + element);
-        if (element === element) {
-            var num = [];
-            num.push(element);
-            console.log('find equal:' + num);
-        }
-    });
-};
-
-//Create e chaining route
+//Create e chaining route to post and return json file
 router.route('/api/friends')
     .post(function(req, res) {
+        //Parse the user data and create a variable to hold user's score and create a variable to calculate the difference
+        var userData = req.body,
+            userScores = userData.scores,
+            totalDifference = 0,
+            bestMatch = {
+                name: '',
+                photo: '',
+                friendDifference: 1000
+            };
 
-        user.push(result);
-        console.log(result);
-        console.log('users ' + i + ' score: ' + req.body.scores);
-
-        for (var count = 0; count < req.body.scores; count++) {
-            console.log('score for loop: ' + req.body[count].scores);
+        for (var i = 0; i < friends.length; i++) {
+            //Loop through friends object
+            console.log(friends[i].name);
+            console.log(friends[i].score);
+            //Loop through user's data and calculate the difference
         }
-        var score = [];
-        score.push(req.body.scores);
-        console.log("score array here: " + score);
-        compareScore(score);
+        //push user Data to friends
+        friends.push(userData);
+        // Return a JSON with the user's bestMatch. 
+        res.json(bestMatch);
     })
     .get(function(req, res) {
-        var resultJson = JSON.stringify(user);
-        res.json(user);
+        res.json(friends);
     });
-i++;
 
 module.exports = router;
--- app/server.js
+++ app/server.js
@@ -1,27 +1,23 @@
 //Require express and body parser
-var express = require('express');
-var bodyParser = require('body-parser');
-
-//Tell app to use express
-var app = express();
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port = process.env.port || 3000;
 
 //BodyParser makes it possible for our server to interpret data sent to it.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-//Create a port variable to use dynamic port 
-var port = process.env.port || 3000;
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 //Adding Routes
-var htmlRoutes = require('./routing/htmlRoutes')(app);
-var apiRoutes = require('./routing/apiRoutes');
+var htmlRoutes = require('./routing/htmlRoutes')(app),
+    apiRoutes = require('./routing/apiRoutes');
 
 //Tell app to use the routes
 app.use('/', apiRoutes);
 
 //Listen to Port
 app.listen(port, function() {
-    console.log("App listening on PORT " + port);
+    console.log('App listening on PORT ' + port);
 });
